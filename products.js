625a9717d3f37f86a3a921e406fb95a8da62c683
zyntravo <jamunajatav20@gmail.com>
2026-08-01 15:43:15 +0530

    Create products.js
    Zyntravo is a product discovery platform sharing trending products, smart shopping ideas, and best deals from around the web.

---
+++ products.js
@@ -0,0 +1,79 @@
+const products = [
+
+
+{
+name:"Smart Kitchen Organizer",
+
+image:"images/product1.jpg",
+
+category:"Kitchen",
+
+rating:"⭐⭐⭐⭐⭐ 4.8",
+
+oldPrice:"₹1999",
+
+price:"₹799",
+
+link:"YOUR-AFFILIATE-LINK"
+
+},
+
+
+
+{
+name:"Premium Beauty Kit",
+
+image:"images/product2.jpg",
+
+category:"Beauty",
+
+rating:"⭐⭐⭐⭐⭐ 4.7",
+
+oldPrice:"₹1499",
+
+price:"₹499",
+
+link:"YOUR-AFFILIATE-LINK"
+
+},
+
+
+
+{
+name:"Wireless Bluetooth Gadget",
+
+image:"images/product3.jpg",
+
+category:"Electronics",
+
+rating:"⭐⭐⭐⭐⭐ 4.9",
+
+oldPrice:"₹2999",
+
+price:"₹1299",
+
+link:"YOUR-AFFILIATE-LINK"
+
+},
+
+
+
+{
+name:"Trending Fashion Watch",
+
+image:"images/product4.jpg",
+
+category:"Fashion",
+
+rating:"⭐⭐⭐⭐ 4.6",
+
+oldPrice:"₹1999",
+
+price:"₹899",
+
+link:"YOUR-AFFILIATE-LINK"
+
+}
+
+
+];
